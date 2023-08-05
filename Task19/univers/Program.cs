b067cee97e5aea553c581fe83a11a5a546bb6110
POMAH <pomka333@mail.ru>
2023-08-05 21:28:33 +0700

    Домашнее задание по C# к семинару №3

---
+++ Task19/univers/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+// Вариант для любого целого положительного числа
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int i = 1;
+int numI = num;
+int leghtNum = 1;
+double maxNum = 0;
+double minNum = 0;
+bool pal = true;
+
+while (numI > 9)
+{
+    leghtNum++;
+    numI /= 10;
+}
+
+if (leghtNum < 2) pal = false;
+
+while (i <= leghtNum / 2)
+{
+    maxNum = Math.Truncate((num % Math.Pow(10, leghtNum - i + 1)) / Math.Pow(10, leghtNum - i));
+    minNum = Math.Truncate((num % Math.Pow(10, i)) / Math.Pow(10, i - 1));
+
+    if (maxNum != minNum) pal = false;
+
+    i++;
+}
+
+if (pal)
+{
+    System.Console.WriteLine($"Введённое число {num} является палиндромом");
+}
+else
+{
+    System.Console.WriteLine($"Введённое число {num} не является палиндромом");
+}
+
---
+++ Task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+Console.WriteLine("Введите координаты точки А ");
+Console.Write("x: ");
+int numAx = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int numAy = int.Parse(Console.ReadLine());
+Console.Write("z: ");
+int numAz = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B ");
+Console.Write("x: ");
+int numBx = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int numBy = int.Parse(Console.ReadLine());
+Console.Write("z: ");
+int numBz = int.Parse(Console.ReadLine());
+
+double numD = Math.Sqrt(Math.Pow((numBx - numAx), 2) + Math.Pow((numBy - numAy), 2) + Math.Pow((numBz - numAz), 2));
+
+Console.WriteLine($"Расстояние между точками A({numAx}, {numAy}, {numAz}) и B({numBx}, {numBy}, {numBz}) равно: {numD}");
+
